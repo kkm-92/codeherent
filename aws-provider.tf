f5bac8082a4c8ab295f86be4e3cf11d1c40a0293
root <kwangmo_koo@tmax.co.kr>
2021-08-23 05:23:24 +0000

    Mon Aug 23 2021 - Committed via Codeherent

--- aws-provider.tf
+++ aws-provider.tf
@@ -1,10 +1,9 @@
 provider "aws" {
   #access_key = ""
   #secret_key = ""
-  region     = "us-east-2"
+  region = "us-east-2"
 }
 
 module "aws-network-configs" {
   source = "./network-configs"
 }
-
